9dc4f7fff29e902968785a91f00b3338f5052108
yogesh <yogeshmaher64@gmail.com>
2017-11-26 21:25:56 +0530

    Emmiting Messages

--- index.js
+++ index.js
@@ -17,4 +17,8 @@ var io = socket(server);
 
 io.on('connection',function(socket){
   console.log('socket connected');
+
+  socket.on('chat',function(data){
+    io.sockets.emit('chat',data);
+  });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,22 @@
 //making connections
 
 var socket = io.connect('http://localhost:4000');
+
+//Query DOM
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+
+//Emit events
+btn.addEventListener('click',function(){
+  socket.emit('chat',{
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+//Listen for events from server
+socket.on('chat', function(data){
+  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
